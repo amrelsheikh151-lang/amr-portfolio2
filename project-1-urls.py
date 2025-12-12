bdce990f0e5ba2e620dd5e30f0ea7550123b267d
amrelsheikh151-lang <amrelsheikh151@gmail.com>
2025-12-12 14:42:49 +0200

    Create project-1-urls.py

---
+++ project-1-urls.py
@@ -0,0 +1,24 @@
+# Project 1: ALOMDa – Home Design Brand Identity UAE
+# Cover: Image 2 (1_2_yeizji.jpg)
+# Gallery: All 12 images
+
+PROJECT_1_COVER = "https://res.cloudinary.com/dwib2lgzp/image/upload/f_auto,q_auto/v1765471448/1_2_yeizji.jpg"
+
+PROJECT_1_GALLERY = [
+    "https://res.cloudinary.com/dwib2lgzp/image/upload/f_auto,q_auto/v1765471458/2_2_m6iy10.jpg",
+    "https://res.cloudinary.com/dwib2lgzp/image/upload/f_auto,q_auto/v1765471448/1_2_yeizji.jpg",
+    "https://res.cloudinary.com/dwib2lgzp/image/upload/f_auto,q_auto/v1765471499/5_xhvb6v.jpg",
+    "https://res.cloudinary.com/dwib2lgzp/image/upload/f_auto,q_auto/v1765471464/4_hdjxna.jpg",
+    "https://res.cloudinary.com/dwib2lgzp/image/upload/f_auto,q_auto/v1765471565/10_cbiuaw.png",
+    "https://res.cloudinary.com/dwib2lgzp/image/upload/f_auto,q_auto/v1765471525/3_lcpqg6.jpg",
+    "https://res.cloudinary.com/dwib2lgzp/image/upload/f_auto,q_auto/v1765471589/8_gczuhe.png",
+    "https://res.cloudinary.com/dwib2lgzp/image/upload/f_auto,q_auto/v1765471617/11_osq7ey.png",
+    "https://res.cloudinary.com/dwib2lgzp/image/upload/f_auto,q_auto/v1765471626/13_rvqoia.png",
+    "https://res.cloudinary.com/dwib2lgzp/image/upload/f_auto,q_auto/v1765471644/9_kxz78e.png",
+    "https://res.cloudinary.com/dwib2lgzp/image/upload/f_auto,q_auto/v1765471684/17_cxut9l.png",
+    "https://res.cloudinary.com/dwib2lgzp/image/upload/f_auto,q_auto/v1765471723/16_c0glqp.png"
+]
+
+print("Project 1 URLs saved!")
+print(f"Cover: {PROJECT_1_COVER}")
+print(f"Gallery: {len(PROJECT_1_GALLERY)} images")
